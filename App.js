805be3c116c7c4f2616828211d536e7f3d6bf006
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-02-01 01:43:57 +0900

    cacheFonts

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
 
 const cacheImages = (images) =>
   images.map((image) => {
@@ -12,14 +14,18 @@ const cacheImages = (images) =>
     }
   });
 
+const cacheFonts = (fonts) =>
+  fonts.map((font) => [Font.loadAsync(font), Font.loadAsync(font)]);
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = cacheImages([
       "https://images.unsplash.com/photo-1611890225664-b7b0ac420b64?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
       require("./assets/splash.png"),
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
   return isReady ? (
