0734943858eed7d6e1c9b1e071fe9fa58c9a4bb7
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-02-04 05:14:31 +0900

    Styling Navigation and Adding the Icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect } from "react";
+import { Platform } from "react-native";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
@@ -13,10 +15,42 @@ const getHeaderName = (route) =>
 
 export default ({ navigation, route }) => {
   useLayoutEffect(() => {
-    navigation.setOptions({ title: getHeaderName(route) });
+    navigation.setOptions({
+      title: getHeaderName(route),
+    });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Movies") {
+            iconName += "film";
+          } else if (route.name === "TV") {
+            iconName += "tv";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favorites") {
+            iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={26}
+              color={focused ? "white" : "grey"}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "white",
+        inactiveTintColor: "grey",
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
