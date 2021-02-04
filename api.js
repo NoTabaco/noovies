c2b3545e888880c68ad717cb0615a08c16bcd7af
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-02-05 06:32:57 +0900

    Updated API Layer

---
+++ api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const TMDB_KEY = "fbd978aee26f62ff878703caa3fed923";
+
+const makeRequest = (path, params) =>
+  axios.get(`https://api.themoviedb.org/3${path}`, {
+    params: {
+      ...params,
+      api_key: TMDB_KEY,
+    },
+  });
+
+export const movieApi = {
+  nowPlaying: () => makeRequest("/movie/now_playing"),
+  popular: () => makeRequest("/movie/popular"),
+  upcoming: () =>
+    makeRequest("/movie/upcoming", {
+      region: "kr",
+    }),
+  search: (query) =>
+    makeRequest("/search/movie", {
+      query,
+      include_adult: true,
+    }),
+  movie: (id) => makeRequest(`/movie/${id}`),
+  discover: () => makeRequest("/discover/movie"),
+};
+
+export const tvApi = {
+  today: () => makeRequest("/tv/airing_today"),
+  thisWeek: () => makeRequest("/tv/on_the_air"),
+  topRated: () => makeRequest("/tv/top_rated"),
+  popular: () => makeRequest("/tv/popular"),
+  search: (query) => makeRequest("/search/tv", { query, include_adult: true }),
+  show: (id) => makeRequest(`/tv/${id}`),
+};
